8ec67152960e0744fc150216a7d3114f9b45dfe9
Mohammed23200 <mgsa112233@gmail.com>
2025-06-14 16:48:30 +0300

    all project

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# treasure-island
-try to find my treasure 
+# 🏝️ Treasure Island Game 🏝️
+
+image.png
+
+## 🎮 About The Project
+This is a fun text-based adventure game created as part of Day 3 of "100 Days of Code" with Dr. Angela Yu. The game challenges players to make choices that will lead them to either find the treasure or face a game over!
+
+## 🎯 Game Features
+- 🧭 Interactive decision-making gameplay
+- 🎲 Multiple endings based on player choices
+- 🎨 ASCII art treasure map
+- 😈 Fun and challenging scenarios
+
+## 🚀 How to Play
+1. Clone the repository
+2. Run the game using Python:
+
+python tearsure.py
+
+1. Make your choices carefully to find the treasure!
+
+## 🎯 Game Rules
+- Choose your path wisely (left or right)
+- Decide whether to swim or wait for a boat
+- Select the correct door color to find the treasure
+
+## 🛠️ Built With
+- Python 3
+- ASCII Art
+- Dr. Angela Yu's "100 Days of Code" Course
+
+## 📚 Learning Outcomes
+- Python input/output handling
+- Conditional statements
+- ASCII art implementation
+- Game logic and flow control
+
+## 🔗 Links
+- [GitHub Repository](https://github.com/yourusername/treasure-island)
+- [100 Days of Code Course](https://www.udemy.com/course/100-days-of-code/)
+
+## 👨‍💻 Author
+Your Name
+- GitHub: [@yourusername](https://github.com/yourusername)
+- LinkedIn: [Your LinkedIn Profile](https://linkedin.com/in/yourprofile)
+
+## 📝 License
+This project is part of the "100 Days of Code" challenge by Dr. Angela Yu.
+
+---
+⭐️ From [Your Name](https://github.com/yourusername) 
--- tearsure.py
+++ tearsure.py
@@ -1,1 +1,41 @@
-print("hello world!")
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+first = input('now choose you want to go left or right 😈')
+if first == 'right':
+    print('hahaha you fall into a hole Game Over 😈')
+else:
+    print("good choice you survived😍 but now we are not done here yet 🙂‍↕️")
+    second = input("now choose you want to swim the lack to go to the island or jus wait for boat 😈")
+    if second == 'swim':
+        print("hahaha 😈 no you attacked by my trout GameOver 😏") 
+    else:
+        print("you survived okay this is a good luck only 😦 but there is one final thing 😈")
+        final =input('you have three doors on is red 🔴 one is yellow 🟡 and one is blue 🔵 choose which one you will enter hero 😈')
+        if (final == 'red'):
+            print("hehehe you enter room full of fire 🔥 game over 😈")
+        elif (final == 'blue'):
+            print("hehehe you eaten by beasts 🧌 game over 😈")
+        else :
+            print("you  win and kill me 🎉 ")
